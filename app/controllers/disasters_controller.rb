6090031a61cede8edcf3f8c5e464ec72cb394e86
jackalwei <jackal8125@gmail.com>
2016-06-17 15:12:27 +0800

    finish disaster CRUD features

--- app/controllers/disasters_controller.rb
+++ app/controllers/disasters_controller.rb
@@ -1,2 +1,55 @@
 class DisastersController < ApplicationController
+
+  before_action :find_disaster, :only => [:show, :edit, :update, :destroy]
+
+  def index
+    @disasters = Disaster.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @disaster.update(set_disaster_params)
+      flash[:notice] = "Edited successfully!!"
+      redirect_to disaster_path(@disaster)
+    else
+      render :action => :edit
+    end
+  end
+
+  def new
+    @disaster = Disaster.new
+  end
+
+  def create
+    @disaster = Disaster.new(set_disaster_params)
+    if @disaster.save
+      flash[:notice] = "New Success!!"
+      redirect_to disasters_path
+    else
+      render :action => :new
+    end
+  end
+
+  def destroy
+    @disaster.destroy
+    redirect_to disasters_path
+  end
+
+
+  #inside  method
+  protected
+
+  def set_disaster_params
+    params.require(:disaster).permit(:name, :description)
+  end
+
+  def find_disaster
+    @disaster = Disaster.find(params[:id])
+  end
+
 end
